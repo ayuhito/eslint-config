c79bd818ab5a1697668fec4626e1ba43657cc66a
DecliningLotus <declininglotus@gmail.com>
2022-06-25 15:33:16 +0300

    fix: patch require

--- patch.js
+++ patch.js
@@ -1,2 +1,3 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-import "@rushstack/eslint-patch/modern-module-resolution";
+// eslint-disable-next-line unicorn/prefer-module
+require("@rushstack/eslint-patch/modern-module-resolution");
