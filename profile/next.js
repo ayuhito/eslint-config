945fae6c93ddbf7cd95619a5634ae4d6378463f2
DecliningLotus <declininglotus@gmail.com>
2022-07-16 11:15:47 +0300

    fix: add import resolvers for next

--- profile/next.js
+++ profile/next.js
@@ -22,6 +22,17 @@ const next = {
     react: {
       version: "detect", // Automatically detect the react version
     },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      [require.resolve('eslint-import-resolver-node')]: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      [require.resolve('eslint-import-resolver-typescript')]: {
+        alwaysTryTypes: true,
+      },
+    },
   },
   overrides: [
     {
